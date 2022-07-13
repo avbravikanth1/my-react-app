4c9f42fe0591d0ea2f3768f6e05128362ea57c8e
ravikanth <ravikanth.ado@gmail.com>
2022-07-13 17:02:59 +0530

    modified h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 	return (
     <div className="App">
-	<h1>Hello World!This is Murali Noori</h1>		
+	<h1>Hello World!This is Murali</h1>		
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
